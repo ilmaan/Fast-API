89fe2df169b226296ab15a70e03262e898419018
ilmaan <ilmancc@gmail.com>
2022-08-17 15:36:49 +0530

    ADDING Database Connection Functionality

--- app/main.py
+++ app/main.py
@@ -5,13 +5,27 @@ from uuid import UUID,uuid4
 from pydantic import BaseModel 
 from random import randrange     
 from fastapi.params import Body
-
+import psycopg2 
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
 class Post(BaseModel): 
     title: str
     content: str
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost',database='fast-api',user='postgres',password='1234',cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Connected to the database")
+        break
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error) 
+        time.sleep(2)
+
+
 
 @app.post("/createpost")
 def create_post(post:Post):
@@ -26,14 +40,21 @@ mposts = [{"id":1,"title":"title1","content":"content1"},{"id":2,"title":"title2
 
 @app.get("/posts")
 def get_posts():
-    return {"posts":mposts}
+    cursor.execute("""SELECT * FROM posts """)
+    posts = cursor.fetchall()
+    print(posts)
+    return {"posts":posts}
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_post(post:Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(1,100)
-    mposts.append(post_dict)
-    return {"posts":mposts}
+    cursor.execute("""INSERT INTO posts (title,content) VALUES (%s,%s) RETURNING * """,(post.title,post.content))
+    new_post = cursor.fetchone()
+    conn.commit()
+
+    # post_dict = post.dict()
+    # post_dict['id'] = randrange(1,100)
+    # mposts.append(post_dict)
+    return {"posts":new_post} 
 
 
 def find_post(id): 
