d66b7fe679bdc603225cbc4aa30098a7b0181ffa
ilmaan <ilmancc@gmail.com>
2022-08-17 12:26:51 +0530

    CRUD With Fast Api

--- main.py
+++ main.py
@@ -1,9 +1,98 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response,status, HTTPException
 from models import User, Gender, Role
-from typing import List
+from typing import List, Optional
 from uuid import UUID,uuid4
+from pydantic import BaseModel 
+from random import randrange     
+from fastapi.params import Body
+
 
 app = FastAPI()
+
+class Post(BaseModel): 
+    title: str
+    content: str
+
+@app.post("/createpost")
+def create_post(post:Post):
+    print(post, type(post))
+    p = post.dict()
+    print(type(p))
+
+    return {"title":post.title,"content":post.content,"p":p}  
+
+
+mposts = [{"id":1,"title":"title1","content":"content1"},{"id":2,"title":"title2","content":"content2"}]
+
+@app.get("/posts")
+def get_posts():
+    return {"posts":mposts}
+
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
+def create_post(post:Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(1,100)
+    mposts.append(post_dict)
+    return {"posts":mposts}
+
+
+def find_post(id): 
+    for post in mposts:
+        if post['id'] == id:
+            print(post)
+            return post
+    return None
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):   
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=404, detail=f"Post not found for id {id}")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"detail":f"post not found for id {id}"} 
+    print(post)
+    return {"posts":id,"post":post}
+
+@app.get("/latestpost")
+def get_latest_post(): 
+    lpost = mposts[-1]
+    print(lpost)
+    return {"posts":lpost}
+
+
+def find_index(id):
+    for p in mposts:
+        if p['id'] == id:
+            print(p,mposts.index(p))
+            return mposts.index(p)
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, response: Response):
+    index = find_index(id)
+    print(index)
+    if not index:
+        raise HTTPException(status_code=404, detail=f"Post not found for id {id}")
+    mposts.pop(index)
+    # return {"posts":mposts,"detail":f"post {id} deleted"}
+    return Response(status_code=status.HTTP_204_NO_CONTENT) 
+
+
+@app.put("/posts/{id}",status_code=status.HTTP_200_OK) 
+def update_post(id: int, post: Post):
+    index = find_index(id)
+    if index == None:
+        raise HTTPException(status_code=404, detail=f"Post not found for id {id}")
+    # mposts[index] = post.dict()
+    post_dict = post.dict()
+    post_dict['id'] = id
+    print(post_dict,'*'*30,post_dict['id'])
+    mposts[index] = post_dict
+    return {"posts":post_dict}
+
+
+
+
+
 
 db: List[User] = [
         User(id=uuid4(),fname='ilmaan',lname='zia',gender=Gender.male,roles=[Role.admin]),
